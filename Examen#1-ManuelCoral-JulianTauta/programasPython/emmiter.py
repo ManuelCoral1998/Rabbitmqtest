1a7a6e8ef9952bd29192cbac67050d29f2d32fd0
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2020-03-21 23:45:38 -0500

    Código Examen # 1 distribuidos

---
+++ Examen#1-ManuelCoral-JulianTauta/programasPython/emmiter.py
@@ -0,0 +1,24 @@
+'''
+Productor de mensajes para:
+Grupo01
+Grupo02
+General
+'''
+
+import pika
+import sys
+
+credentials = pika.PlainCredentials('Productor', 'Productor')
+#connection = pika.BlockingConnection(
+#    pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.56.12',5672,'/',credentials))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+
+group = sys.argv[1] if len(sys.argv) > 1 else 'General'
+message = ' '.join(sys.argv[2:]) or 'Hello World!'
+channel.basic_publish(
+    exchange='direct_logs', routing_key=group, body=message)
+print(" [x] Sent %r:%r" % (group, message))
+connection.close()
---
+++ Examen#1-ManuelCoral-JulianTauta/programasPython/receiver1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+"""
+Consumidor1 de:
+ -Grupo01
+ -General
+"""
+
+import pika
+import sys
+credentials = pika.PlainCredentials('Consumidor1', 'Consumidor1')
+#connection = pika.BlockingConnection(
+#    pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.56.12',5672,'/',credentials))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+#severities = sys.argv[1:]
+groups = ["Grupo01", "General"]
+if not groups:
+    sys.stderr.write("Usage: %s [info] [warning] [error]\n" % sys.argv[0])
+    sys.exit(1)
+
+for group in groups:
+    channel.queue_bind(
+        exchange='direct_logs', queue=queue_name, routing_key=group)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
